12e2eab85aec45cfde4fdfdbc024615311a47333
Lindsay Spencer <lindsay.m.spencer@gmail.com>
2019-09-18 08:16:53 -0700

    Project 1 - Adding labels to the form.

--- MatchingPairsGame/MatchingPairsGame/Form1.Designer.cs
+++ MatchingPairsGame/MatchingPairsGame/Form1.Designer.cs
@@ -29,6 +29,23 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanelMatchingPairsGame = new System.Windows.Forms.TableLayoutPanel();
+            this.label00 = new System.Windows.Forms.Label();
+            this.label01 = new System.Windows.Forms.Label();
+            this.label02 = new System.Windows.Forms.Label();
+            this.label03 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label30 = new System.Windows.Forms.Label();
+            this.label31 = new System.Windows.Forms.Label();
+            this.label32 = new System.Windows.Forms.Label();
+            this.label33 = new System.Windows.Forms.Label();
+            this.tableLayoutPanelMatchingPairsGame.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanelMatchingPairsGame
@@ -40,6 +57,22 @@
             this.tableLayoutPanelMatchingPairsGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanelMatchingPairsGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanelMatchingPairsGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label33, 3, 3);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label32, 2, 3);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label31, 1, 3);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label30, 0, 3);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label23, 3, 2);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label22, 2, 2);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label21, 1, 2);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label20, 0, 2);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label13, 3, 1);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label12, 2, 1);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label11, 1, 1);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label10, 0, 1);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label03, 3, 0);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label02, 2, 0);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label01, 1, 0);
+            this.tableLayoutPanelMatchingPairsGame.Controls.Add(this.label00, 0, 0);
             this.tableLayoutPanelMatchingPairsGame.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanelMatchingPairsGame.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanelMatchingPairsGame.Name = "tableLayoutPanelMatchingPairsGame";
@@ -51,6 +84,182 @@
             this.tableLayoutPanelMatchingPairsGame.Size = new System.Drawing.Size(528, 494);
             this.tableLayoutPanelMatchingPairsGame.TabIndex = 0;
             // 
+            // label00
+            // 
+            this.label00.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label00.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label00.Location = new System.Drawing.Point(5, 2);
+            this.label00.Name = "label00";
+            this.label00.Size = new System.Drawing.Size(123, 121);
+            this.label00.TabIndex = 0;
+            this.label00.Text = "c";
+            this.label00.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label01
+            // 
+            this.label01.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label01.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label01.Location = new System.Drawing.Point(136, 2);
+            this.label01.Name = "label01";
+            this.label01.Size = new System.Drawing.Size(123, 121);
+            this.label01.TabIndex = 1;
+            this.label01.Text = "c";
+            this.label01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label02
+            // 
+            this.label02.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label02.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label02.Location = new System.Drawing.Point(267, 2);
+            this.label02.Name = "label02";
+            this.label02.Size = new System.Drawing.Size(123, 121);
+            this.label02.TabIndex = 2;
+            this.label02.Text = "c";
+            this.label02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label03
+            // 
+            this.label03.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label03.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label03.Location = new System.Drawing.Point(398, 2);
+            this.label03.Name = "label03";
+            this.label03.Size = new System.Drawing.Size(125, 121);
+            this.label03.TabIndex = 3;
+            this.label03.Text = "c";
+            this.label03.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label10.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label10.Location = new System.Drawing.Point(5, 125);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(123, 121);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "c";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label11.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label11.Location = new System.Drawing.Point(136, 125);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(123, 121);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "c";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label12.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label12.Location = new System.Drawing.Point(267, 125);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(123, 121);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "c";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            this.label13.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label13.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label13.Location = new System.Drawing.Point(398, 125);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(125, 121);
+            this.label13.TabIndex = 7;
+            this.label13.Text = "c";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label20
+            // 
+            this.label20.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label20.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label20.Location = new System.Drawing.Point(5, 248);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(123, 121);
+            this.label20.TabIndex = 8;
+            this.label20.Text = "c";
+            this.label20.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label21
+            // 
+            this.label21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label21.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label21.Location = new System.Drawing.Point(136, 248);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(123, 121);
+            this.label21.TabIndex = 9;
+            this.label21.Text = "c";
+            this.label21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label22
+            // 
+            this.label22.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label22.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label22.Location = new System.Drawing.Point(267, 248);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(123, 121);
+            this.label22.TabIndex = 10;
+            this.label22.Text = "c";
+            this.label22.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label23
+            // 
+            this.label23.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label23.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label23.Location = new System.Drawing.Point(398, 248);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(125, 121);
+            this.label23.TabIndex = 11;
+            this.label23.Text = "c";
+            this.label23.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label30
+            // 
+            this.label30.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label30.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label30.Location = new System.Drawing.Point(5, 371);
+            this.label30.Name = "label30";
+            this.label30.Size = new System.Drawing.Size(123, 121);
+            this.label30.TabIndex = 12;
+            this.label30.Text = "c";
+            this.label30.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label31
+            // 
+            this.label31.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label31.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label31.Location = new System.Drawing.Point(136, 371);
+            this.label31.Name = "label31";
+            this.label31.Size = new System.Drawing.Size(123, 121);
+            this.label31.TabIndex = 13;
+            this.label31.Text = "c";
+            this.label31.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label32
+            // 
+            this.label32.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label32.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label32.Location = new System.Drawing.Point(267, 371);
+            this.label32.Name = "label32";
+            this.label32.Size = new System.Drawing.Size(123, 121);
+            this.label32.TabIndex = 14;
+            this.label32.Text = "c";
+            this.label32.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label33
+            // 
+            this.label33.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label33.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label33.Location = new System.Drawing.Point(398, 371);
+            this.label33.Name = "label33";
+            this.label33.Size = new System.Drawing.Size(125, 121);
+            this.label33.TabIndex = 15;
+            this.label33.Text = "c";
+            this.label33.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // FormMatchingPairsGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -59,6 +268,7 @@
             this.Controls.Add(this.tableLayoutPanelMatchingPairsGame);
             this.Name = "FormMatchingPairsGame";
             this.Text = "Matching Pairs Game";
+            this.tableLayoutPanelMatchingPairsGame.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -66,6 +276,22 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMatchingPairsGame;
+        private System.Windows.Forms.Label label00;
+        private System.Windows.Forms.Label label33;
+        private System.Windows.Forms.Label label32;
+        private System.Windows.Forms.Label label31;
+        private System.Windows.Forms.Label label30;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label03;
+        private System.Windows.Forms.Label label02;
+        private System.Windows.Forms.Label label01;
     }
 }
 
